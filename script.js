4debb9ea98ada8accb8ca08c762f3ebade1ecdc5
Justin Dionisio <justindionisio21@yahoo.com>
2020-03-03 21:34:01 -0800

    Change wikipedia API

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const mod = (function() {
+const mod = (function () {
   var input = document.querySelector(".input");
 
   function loadText(e) {
@@ -9,40 +9,27 @@ const mod = (function() {
     document.getElementById("loadingText").style.display = "block";
 
     // Wiki API
-    var endpoint = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${
-      input.value
-    }&format=json&origin=*`;
+    var endpoint = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${input.value}&origin=*`
 
     //  Make AJAX Request
     var data = fetch(endpoint)
       .then(res => res.json())
       .then(data => {
         document.getElementById("loadingText").style.display = "none";
-        var foo = data.filter(function(item, index) {
-          return index !== 0;
-        });
 
-        var titlesArr = foo[0];
-        var descriptionArr = foo[1];
-        var linksArr = foo[2];
-
-        // Iterate through resultsArr. For each result, store them in an <li> tag.
-        var markup = titlesArr
-          .map(function(title, index) {
-            return `
-      <li class="row">
-        <a href="${linksArr[index]}" target="_blank">
-          <h1 class="title"{>${titlesArr[index]}</h1>
-          <p>${descriptionArr[index]}</p>
-        </a>
-      </li>
-    `;
-          })
+        var markup = data.query.search.map(function (article) {
+          return `<li class="row">
+            <a href="#" target="_blank">
+              <h1 class="title">${article.title}</h1>
+              <p>${article.snippet}</p>
+            </a>
+          </li>`
+        })
           .join("");
 
         document.getElementById("output").innerHTML = markup;
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.log(err);
       });
   }
